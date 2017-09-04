243fd594574f7071b68d3b8cc1ee361dfc6d77c4
Placid Rodrigues <placidrod@gmail.com>
2017-09-04 01:24:15 -0700

    JSX Intro

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
+// const ce = React.createElement;
 
 const MyTitle = function MyTitle(props) {
-  return ce('div', null, ce('h2', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function MyFirstComponent() {
-  return ce(
-    'div',
-    null,
-    ce('h1', null, 'My first component'),
-    ce(MyTitle, { title: 'Friends', color: 'peru' }),
-    ce(MyTitle, { title: 'Breaking Bad', color: 'tomato' }),
-    ce(MyTitle, { title: 'Big Bang Theory', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Sherlock', color: '#666' })
+  // return ce('div', null, ce('h2', { style: { color: props.color } }, props.title));
+  return (
+    <div>
+      <h2 style={{ color: props.color }}>{props.title}</h2>
+    </div>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+const MyFirstComponent = function MyFirstComponent() {
+  return (
+    <div id="my-first-component">
+      <MyTitle title="Friends" color="peru" />
+      <MyTitle title="Breaking Bad" color="tomato" />
+      <MyTitle title="Big Bang Theory" color="YellowGreen" />
+      <MyTitle title="Sherlock" color="#666" />
+    </div>
+  );
+
+  // return ce(
+  //   'div',
+  //   null,
+  //   ce('h1', null, 'My first component'),
+  //   ce(MyTitle, { title: 'Friends', color: 'peru' }),
+  //   ce(MyTitle, { title: 'Breaking Bad', color: 'tomato' }),
+  //   ce(MyTitle, { title: 'Big Bang Theory', color: 'YellowGreen' }),
+  //   ce(MyTitle, { title: 'Sherlock', color: '#666' })
+  // );
+};
+
+render(<MyFirstComponent />, document.getElementById('app'));
