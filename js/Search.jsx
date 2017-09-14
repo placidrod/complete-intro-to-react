b4d478961e704ad9a168442306dc6ef58c01b12d
Placid Rodrigues <placidrod@gmail.com>
2017-09-14 21:27:44 +0600

    Flow before changing ShowCard

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
@@ -14,7 +16,7 @@ class Search extends Component {
     searchTerm: ''
   };
 
-  handleSearchTermChange = event => {
+  handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
     this.setState({
       searchTerm: event.target.value
     });
