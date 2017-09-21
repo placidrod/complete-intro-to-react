45b2f2aa178edf3355c12cf7aa9b035a247c51ed
Placid Rodrigues <placidrod@gmail.com>
2017-09-21 13:24:03 +0600

    Details component with pre tag

---
+++ js/Details.jsx
@@ -0,0 +1,16 @@
+// @flow
+
+import React from 'react';
+
+const Details = (props: { show: Show }) => {
+  const { title, description, year, poster, trailer } = props.show;
+  return (
+    <div className="details">
+      <pre>
+        <code>{JSON.stringify(props, null, 4)}</code>
+      </pre>
+    </div>
+  );
+};
+
+export default Details;
