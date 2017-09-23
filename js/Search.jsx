2a9ea6519baa97f7d53a8867fd5359f73c57447d
Placid Rodrigues <placidrod@gmail.com>
2017-09-23 22:29:54 +0600

    Using Redux in Search Component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,48 +1,36 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import ShowCard from './ShowCard';
 import Header from './Header';
 
-class Search extends Component {
-  state = {
-    searchTerm: ''
-  };
+const Search = (props: {
+  searchTerm: string, // eslint-disable-line react/no-unused-prop-types
+  shows: Array<Show>
+}) => (
+  <div className="search">
+    <Header showSearch />
+    <div>
+      {props.shows
+        .filter(show => `${show.title} ${show.description}`.toUpperCase().includes(props.searchTerm.toUpperCase()))
+        .map(show => (
+          <ShowCard
+            key={show.imdbID}
+            title={show.title}
+            poster={show.poster}
+            year={show.year}
+            description={show.description}
+            trailer={show.trailer}
+            imdbID={show.imdbID}
+          />
+        ))}
+    </div>
+  </div>
+);
 
-  props: {
-    shows: Array<Show>
-  };
+const mapStateToProps = state => ({
+  searchTerm: state.searchTerm
+});
 
-  handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
-    this.setState({
-      searchTerm: event.target.value
-    });
-  };
-
-  render() {
-    return (
-      <div className="search">
-        <Header showSearch searchTerm={this.state.searchTerm} handleSearchTermChange={this.handleSearchTermChange} />
-        <div>
-          {this.props.shows
-            .filter(show =>
-              `${show.title} ${show.description}`.toUpperCase().includes(this.state.searchTerm.toUpperCase())
-            )
-            .map(show => (
-              <ShowCard
-                key={show.imdbID}
-                title={show.title}
-                poster={show.poster}
-                year={show.year}
-                description={show.description}
-                trailer={show.trailer}
-                imdbID={show.imdbID}
-              />
-            ))}
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Search;
+export default connect(mapStateToProps)(Search);
