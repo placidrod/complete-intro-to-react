2c1ddf16347bbc9982bd2414fdf299f84bcc8a7a
Placid Rodrigues <placidrod@gmail.com>
2017-09-26 20:10:54 +0600

    Before Async Redux

--- js/reducers.js
+++ js/reducers.js
@@ -3,7 +3,7 @@
 import { combineReducers } from 'redux';
 import { SET_SEARCH_TERM } from './actions';
 
-const searchTerm = (state: '', action: Action) => {
+const searchTerm = (state = '', action: Action) => {
   if (action.type === SET_SEARCH_TERM) {
     return action.payload;
   }
