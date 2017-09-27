dd02b6d69b67a3c46d5a5de3187380a5d11c298e
Placid Rodrigues <placidrod@gmail.com>
2017-09-27 14:56:50 +0600

    Before 'Dispatching Actions' under Testing Redux

--- js/__test__/Search.spec.jsx
+++ js/__test__/Search.spec.jsx
@@ -3,23 +3,23 @@
 import React from 'react';
 import { shallow } from 'enzyme'; // eslint-disable-line import/no-extraneous-dependencies
 import preload from '../../data.json';
-import Search from '../Search';
+import Search, { Unwrapped as UnwrappedSearch } from '../Search';
 import ShowCard from '../ShowCard';
 
 test('Search renders correctly', () => {
-  const component = shallow(<Search shows={preload.shows} />);
+  const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm="" />);
   expect(component).toMatchSnapshot();
 });
 
 test('Search should render correct amount of shows', () => {
-  const component = shallow(<Search shows={preload.shows} />);
+  const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm="" />);
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
 test('Search should render correct amount of shows based on search term', () => {
   const searchTerm = 'black';
-  const component = shallow(<Search shows={preload.shows} />);
-  component.find('input').simulate('change', { target: { value: searchTerm } });
+  const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm={searchTerm} />);
+  // component.find('input').simulate('change', { target: { value: searchTerm } });
   const showCount = preload.shows.filter(show =>
     `${show.title} ${show.description}`.toUpperCase().includes(searchTerm.toUpperCase())
   ).length;
